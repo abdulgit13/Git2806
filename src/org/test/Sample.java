362fae052ce6d0e9749edfc991850ec8ebaeeedc
abdulgit13 <abdulzameer71@gmail.com>
2022-06-28 21:08:53 +0530

    testone

---
+++ src/org/test/Sample.java
@@ -0,0 +1,41 @@
+package org.test;
+
+import java.util.Collection;
+import java.util.Hashtable;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+public class Sample {
+	public static void main(String[] args) {
+		
+Map<String, String> m=new Hashtable();
+
+m.put("vel", "selenium");
+m.put("ganesh", "python");
+m.put("dinesh", "corejava");
+m.put("subash", "jira");
+m.put("vengat","framework");
+
+
+
+Collection<String> values = m.values();
+System.out.println(values);
+
+Set<Entry<String,String>> set = m.entrySet();
+
+for (Entry<String, String> entry : set) {
+	System.out.println(entry);
+	
+}
+ Set<String> keySet = m.keySet();
+ 
+ for (String string : keySet) {
+	 
+	 System.out.println(keySet);
+	
+}
+	
+	
+}
+}
